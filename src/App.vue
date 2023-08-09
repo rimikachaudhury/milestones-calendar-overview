2514ea3c3da82d4189331a70680919bf5e0012d0
chaudhury.rimika <chaudhury.rimika@gmail.com>
2023-08-09 09:41:37 -0700

    basic view of vue based calendar

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import VueCal from 'vue-cal'
+import 'vue-cal/dist/vuecal.css'
+</script>
+
+
+
+<template>
+  <header>
+    <div class="wrapper"></div>
+  </header>
+
+  <main>
+    <vue-cal class="vuecal--blue-theme"></vue-cal>
+  </main>
+</template>
+
+
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.vuecal {height: 90vh;}
+</style>
